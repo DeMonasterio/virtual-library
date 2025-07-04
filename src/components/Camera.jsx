5c2023166eae91d65e56cfabdd91eb4fb5df17dc
de Monasterio <112005353+DeMonasterio@users.noreply.github.com>
2025-07-04 10:16:41 -0300

    Delete src directory

--- src/components/Camera.jsx
+++
@@ -1,7 +0,0 @@
-import * as THREE from "three";
-export const createCamera = (fov) =>{
-
-    const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-    return camera;    
-}
